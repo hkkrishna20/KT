e43453240ce583f52855092579b8c7a6c31998b7
Hari Krishna Manupadi <harikrishnacsse@gmail.com>
2019-07-30 09:39:06 +0530

    Create EmployeeDao.java

---
+++ EmployeeDao.java
@@ -0,0 +1,42 @@
+package com.howtodoinjava.demo.dao;
+ 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+ 
+import org.springframework.stereotype.Repository;
+ 
+import com.howtodoinjava.demo.model.EmployeeVO;
+ 
+@Repository
+public class EmployeeDao {
+     
+    private Map<Integer, EmployeeVO> DB = new HashMap<>();
+     
+    public List<EmployeeVO> getEmployeeList()
+    {
+        List<EmployeeVO> list = new ArrayList<>();
+        if(list.isEmpty()) {
+            list.addAll(DB.values());
+        }
+        return list;
+    }
+     
+    public EmployeeVO getEmployeeById(int id) {
+        return DB.get(id);
+    }
+     
+    public void addEmployee(EmployeeVO employee) {
+        employee.setEmployeeId(DB.keySet().size() + 1);
+        DB.put(employee.getEmployeeId(), employee);
+    }
+     
+    public void updateEmployee(EmployeeVO employee) {
+        DB.put(employee.getEmployeeId(), employee);
+    }
+     
+    public void deleteEmployee(int id) {
+        DB.remove(id);
+    }
+}
